27651fa438ea25e14bf3e6e29762c23e31abc6de
Chhaya Patole <chhayapatole@gmail.com>
2023-08-18 15:47:22 +0530

    Add files via upload

--- MI.py
+++ MI.py
@@ -1,20 +1,18 @@
-
+import pickle
 import pandas as pd
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
 import streamlit as st
 
-
-
+model=pickle.load(open('MI_model.pkl','rb'))
 
 if __name__ == "__main__":
-    st.title("Myocardial Infarction Prediction")
+    st.title("Myocardial Infarction Prediction with ML")
+# user input
     AGE = st.sidebar.number_input("Insert the Age")
     SEX=st.selectbox("Gender", ["0:Female", "1:Male"], index=0)
-    INF_ANAM=st.selectbox("Quantity of myocardial infarctions",["0:Zero", "1:One", "2:Two", "3: Three or more"], index=0)
+    INF_ANAM=st.selectbox("Quantity of myocardial infarctions",["0:Zero", "1: One", "2:Two", "3: Three or more"], index=0)
     STENOK_AN=st.selectbox("Exertional angina pectoris",  ["0: never"," 1: During the last year", "2: one year ago", "3: two years ago", "4: three years ago", "5: 4-5 years ago"], index=0)
-    FK_STENOK=st.selectbox("Functional class (FC) of angina pectoris in the last year",  ["0: there is no angina pectoris", "1: I FC", "2:   II FC", "3: III FC", "4: IV FC"], index=0)
+    FK_STENOK=st.selectbox("Functional class (FC) of angina pectoris in the last year",  ["0: there is no angina pectoris", "1: I FC", "2: II FC", "3: III FC", "4: IV FC"], index=0)
     IBS_POST=st.selectbox("Coronary heart disease (CHD) in recent weeks, days before admission to hospital",
             ["0: none", "1: exertional angina pectoris", "2: unstable angina pectoris"],
             index=0)
@@ -43,8 +41,7 @@ if __name__ == "__main__":
     ritm_ecg_p_01=st.selectbox(
             "ECG rhythm at the time of admission to hospital: sinus (with a heart rate 60-90)",
             ["0:No", "1:Yes"],index=0,)
-    ritm_ecg_p_02=st.selectbox(
-            "ECG rhythm at the time of admission to hospital: atrial fibrillation", ["0:No", "1:Yes"], index=0)
+    ritm_ecg_p_02=st.selectbox("ECG rhythm at the time of admission to hospital: atrial fibrillation", ["0:No", "1:Yes"], index=0)
     n_r_ecg_p_02=st.selectbox(
             "Frequent premature atrial contractions on ECG at the time of admission to hospital",
             ["0:No", "1:Yes"],index=0)
@@ -66,60 +63,56 @@ if __name__ == "__main__":
 
 # Missing Value Imputation
 df_f = df.apply(lambda x: x.fillna(x.mode().iloc[0]))
-             
-# Split the data set
-X = df_f.drop('LET_IS', axis=1)
-Y = df_f['LET_IS']
 
 
-# split into train and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42, stratify=Y)
-
-# Check the class distribution
-class_counts = Y_train.value_counts()
-# Set the desired number of neighbors for SMOTE
-k_neighbors = min(class_counts.min() - 1, 4)  # Use at most class counts - 1
-
-# Check if there are enough samples for SMOTE
-if k_neighbors < 2:
-    print("Not enough samples in the minority class for SMOTE.")
-else:
-    # Create the SMOTE oversampler
-    oversampler = SMOTE(random_state=42, k_neighbors=k_neighbors)
-
-# Perform oversampling on the cleaned dataset
-X_trainS, Y_trainS = oversampler.fit_resample(X_train, Y_train)
-
-
-# Model build
-# Define the model
-model = RandomForestClassifier(
-    n_estimators=100, random_state=42)
-
-
-# Fit the model
-model.fit(X_trainS, Y_trainS)
-predict = model.predict(X_test)
+# separate numeric and categorical columns
+numeric=df_f[['AGE','S_AD_ORIT', 'D_AD_ORIT', 'K_BLOOD','NA_BLOOD','ALT_BLOOD','AST_BLOOD','L_BLOOD','ROE']]
+categorical_columns=[]
+for i in df_f:
+    if i not in numeric:
+        categorical_columns.append(i)
         
-       
+categorical_data = df_f[categorical_columns]
+             
+# Normalise the data
+column_name=numeric.columns
+scaler=preprocessing.MinMaxScaler()
+numeric_norm=scaler.fit_transform(numeric)
+numeric_norm=pd.DataFrame(numeric_norm,columns=column_name)
 
-st.subheader('Predict')
-st.write(predict)
-if(predict == 0):
-    st.write('lethal outcome - :green[unknown (Alive)]')
-elif(predict == 1):
-    st.write('lethal outcome - :red[Cardiogenic Shock]')
-elif(predict == 2):
-    st.write('lethal outcome - :red[Pulmonary Edema]')
-elif(predict == 3):
-    st.write('lethal outcome - :red[Myocardial Rupture]') 
-elif(predict == 4):
-    st.write('lethal outcome - :red[Progress of Congestive Heart Failure]')
-elif(predict == 5):
-    st.write('lethal outcome - :red[Thromboembolism]')
-elif(predict == 6):
-    st.write('lethal outcome - :red[Asystole]')
-elif(predict == 7):
-    st.write('lethal outcome - :red[ventricular fibrillation]')
+
+# best feature
+
+X=df_f['SEX', 'INF_ANAM', 'STENOK_AN', 'FK_STENOK', 'IBS_POST', 'GB', 'nr_02', 'nr_07', 'endocr_02', 'zab_leg_02', 'lat_im', 'inf_im', 'post_im', 'IM_PG_P', 'ritm_ecg_p_01', 'ritm_ecg_p_02', 'n_r_ecg_p_02', 'n_r_ecg_p_08', 'GIPO_K', 'TIME_B_S', 'NOT_NA_1_n', 'RAZRIV']
+
+
+           
+             
+# # code for Prediction
+Outcome= ''
+    
+# creating a button for Prediction    
+if st.button("Prediction"):
+   MI_prediction = model.predict([X]) 
+        
+   if (MI_prediction == 0):
+       Outcome = "Unknown Cause (Alive)"
+   elif(MI_prediction==1):
+       Outcome = "Cardiogenic Shock"
+   elif(MI_prediction==2):
+       Outcome = "Pulmonary Edema"
+   elif(MI_prediction==3):
+       Outcome = "Myocardial Rupture"
+   elif(MI_prediction==4):
+       Outcome = "Progress of Congestive Heart Failure"
+   elif(MI_prediction==5):
+       Outcome = "Thromboembolism"
+   elif(MI_prediction==6):
+       Outcome = "Asystole"
+   elif(MI_prediction==7):
+       Outcome = "Ventricular Fibrillation"
+        
+st.success(MI_prediction)
+
 
 
